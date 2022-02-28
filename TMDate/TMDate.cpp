755d60b601ba655e5d4ede4982fbd35b60f6ff4f
Immanuel-Beena <athanhuntak@gmail.com>
2022-02-28 12:22:29 +0530

    exracting Today date from systemDate successfully Implmented

--- TMDate/TMDate.cpp
+++ TMDate/TMDate.cpp
@@ -1,7 +1,48 @@
 #include<iostream>
+#include<time.h>
 using namespace std;
+class TMDate
+{
+private:
+int dayOfMonth;
+int month;
+int year;
+public:
+TMDate();
+int getDayOfMonth();
+int getMonth();
+int getYear();
+};
+TMDate::TMDate()
+{
+time_t x;
+time(&x);
+struct tm *now;
+now=localtime(&x);
+this->dayOfMonth=now->tm_mday;
+this->month=now->tm_mon+1;
+this->year=now->tm_year+1900;
+}
+int TMDate::getDayOfMonth()
+{
+return  this->dayOfMonth;
+}
+int TMDate::getMonth()
+{
+return this->month;
+}
+int TMDate::getYear()
+{
+return this->year;
+}
+ostream & operator<<(ostream &mout,TMDate &tmdate)
+{
+mout<<tmdate.getDayOfMonth()<<"/"<<tmdate.getMonth()<<"/"<<tmdate.getYear()<<endl;
+return mout;
+}
 int main()
 {
-
+TMDate date;
+cout<<date;
 return 0;
 }
