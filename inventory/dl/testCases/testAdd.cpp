d1ebdc86cb055bf7762d1afcee6e52becfc756db
Immanuel-Beena <athanhuntak@gmail.com>
2022-05-14 16:31:19 +0530

    Testing successfully done

--- inventory/dl/testCases/testAdd.cpp
+++ inventory/dl/testCases/testAdd.cpp
@@ -7,11 +7,12 @@ using namespace inventory;
 using namespace data_layer;
 int main()
 {
-UnitOfMeasurement data(0,"KG");
+UnitOfMeasurement data(0,"ABCDEFGHIJKLMNOPQRSTUWZXYABCDEFGHIJKLMNOPQRSTUWZXYABCDEFGHIJKLMNOPQRSTUWZXYABCDEFGHIJKLMNOPQRSTUWZXYABCDEFGHIJKLMNOPQRSTUWZXY");
 UnitOfMeasurementDAO m;
 try
 {
 m.add(&data);
+cout<<"Unit of Measurement added with code as: "<<data.getCode()<<endl;
 }
 catch(DAOException &err)
 {
