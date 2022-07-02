0f3f8e6fbbdd25b0c6f959c69f200f9199ac39c3
Immanuel-Beena <athanhuntak@gmail.com>
2022-07-02 12:57:34 +0530

    Implementing open,close executeInser method in wrapper of sqlite

---
+++ Sqlite/src/sqlite.cpp
@@ -0,0 +1,128 @@
+#include<sqlite>
+#include<sqlite3.h>
+#include<iostream>
+#include<common/stringutils>
+using namespace std;
+using namespace sqlite;
+using namespace stringutils;
+SqliteDB::SqliteDB() throw (SQLiteException)
+{
+this->db=NULL;
+this->FILE_NAME="";
+}
+SqliteDB::SqliteDB(const SqliteDB &other) throw (SQLiteException)
+{
+int resultCode;
+string error;
+if(other.db==NULL)
+{
+this->db=NULL;
+this->FILE_NAME="";
+return;
+}
+resultCode=sqlite3_open(other.FILE_NAME.c_str(),&(this->db));
+if(resultCode!=SQLITE_OK)
+{
+error=sqlite3_errmsg(this->db);
+sqlite3_close(this->db);
+this->db=NULL;
+this->FILE_NAME="";
+throw SQLiteException(string("unable to connect,reason: ")+error);
+}
+this->FILE_NAME=other.FILE_NAME;
+}
+SqliteDB::SqliteDB(const char *fileName) throw (SQLiteException)
+{
+int resultCode;
+string error;
+resultCode=sqlite3_open(fileName,&(this->db));
+if(resultCode!=SQLITE_OK)
+{
+error=sqlite3_errmsg(this->db);
+sqlite3_close(this->db);
+this->db=NULL;
+this->FILE_NAME="";
+throw SQLiteException("unable to connect,reason: "+error);
+}
+this->FILE_NAME=fileName;
+}
+SqliteDB::SqliteDB(string &fileName) throw (SQLiteException)
+{
+int resultCode;
+string error;
+resultCode=sqlite3_open(fileName.c_str(),&(this->db));
+if(resultCode!=SQLITE_OK)
+{
+error=sqlite3_errmsg(this->db);
+sqlite3_close(this->db);
+this->db=NULL;
+this->FILE_NAME="";
+throw SQLiteException("unable to connect,reason: "+error);
+}
+this->FILE_NAME=fileName;
+}
+SqliteDB::~SqliteDB() throw(SQLiteException)
+{
+if(this->db!=NULL)
+{
+sqlite3_close(this->db);
+this->db=NULL;
+this->FILE_NAME="";
+}
+}
+void SqliteDB::executeInsert(const char *sql) throw(SQLiteException)
+{
+string error;
+char *errorMessage;
+int resultCode,sqlLength;
+string vsql=trimmed(string(sql));
+sqlLength=vsql.length();
+if(sqlLength==0 || sqlLength <= 11) throw SQLiteException("Invalid SQL Statement");
+if(this->db==NULL) throw SQLiteException("Unable to insert because no connection to database");
+resultCode=sqlite3_exec(this->db,vsql.c_str(),0,0,&errorMessage);
+if(resultCode!=SQLITE_OK)
+{
+error=errorMessage;
+sqlite3_free(errorMessage);
+throw SQLiteException(string("unable to insert, reason: ")+error);
+}
+}
+void SqliteDB::executeInsert(const string &sql) throw(SQLiteException)
+{
+string error;
+char *errorMessage;
+int resultCode,sqlLength;
+string vsql=trimmed(sql);
+sqlLength=vsql.length();
+if(sqlLength==0 || sqlLength <= 11) throw SQLiteException("Invalid SQL Statement");
+if(this->db==NULL) throw SQLiteException("Unable to insert because no connection to database");
+resultCode=sqlite3_exec(this->db,vsql.c_str(),0,0,&errorMessage);
+if(resultCode!=SQLITE_OK)
+{
+error=errorMessage;
+sqlite3_free(errorMessage);
+throw SQLiteException(string("unable to insert, reason: ")+error);
+}
+}
+void SqliteDB::close() throw (SQLiteException)
+{
+if(this->db!=NULL)
+{
+sqlite3_close(this->db);
+this->db=NULL;
+this->FILE_NAME="";
+}
+}
+void SqliteDB::open(const char *fileName) throw (SQLiteException)
+{
+string error;
+int resultCode;
+if(this->db!=NULL) this->close();
+resultCode=sqlite3_open(fileName,&(this->db));
+if(resultCode!=SQLITE_OK)
+{
+error=sqlite3_errmsg(this->db);
+this->db=NULL;
+throw SQLiteException(string("unable to connect with database, reason: ")+error);
+}
+}
